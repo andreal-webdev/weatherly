3abfa117915c7a2d421fcb570b3556067e2d03e8
female-bocelli <146786813+female-bocelli@users.noreply.github.com>
2024-03-15 18:52:37 -0400

    geolocation added

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,20 @@
 <script setup>
-  import { ref } from 'vue';
-  import searchBar from './components/searchBar.vue';
-  import cardWeather from './components/cardWeather.vue';
+import { ref } from 'vue';
+import searchBar from './components/searchBar.vue'; // Adjusted component names
+import cardWeather from './components/cardWeather.vue'; // Adjusted component names
 
-  
-  const places = ref([])
-  const addPlace = (data) => {
-    places.value.push(data)
+import userLocation from './components/userLocation.vue'; // Adjusted component name
+
+const places = ref([])
+const addPlace = (data) => {
+  places.value.push(data)
+}
+
+const deleteCity = (name) => {
+  if (confirm('Are you sure?')) {
+    places.value = places.value.filter((p) => p.location.name !== name)
   }
-
-  const deleteCity = (name) => {
-    if (confirm('Are you sure?')) {
-      places.value = places.value.filter((p) => p.location.name !== name)
-    }
-  }
-
+}
 </script>
 
 <template>
@@ -22,19 +22,25 @@
 
     <!-- Navigation Bar -->
     <nav class=" bg-[#093358] flex items-center justify-between text-white text-[16px] py-2 px-3 mb-20">
-          <a class="" href="index.html"><img src="./assets/logo-blue-bg.png" alt="" width="60"></a>
-           <!--Current date-->
-          <div class="text-center ">
-            <i class="fa-regular fa-calendar text-[#24ceff]"></i>
-            {{
-              new Date().toLocaleDateString('en-us', {
-                weekday: 'long',
-                year: 'numeric',
-                month: 'long',
-                day: 'numeric'
-              })
-            }}
-          </div>
+      <a class="" href="index.html"><img src="./assets/logo-blue-bg.png" alt="" width="60"></a>
+        <!--Current date-->
+      <div class="text-center ">
+        <i class="fa-regular fa-calendar text-[#24ceff]"></i>
+        {{
+          new Date().toLocaleDateString('en-us', {
+            weekday: 'long',
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric'
+          })
+        }}
+      </div>
+      <div class="text-white">
+            
+        <userLocation />
+      </div>
+     
+      
     </nav>
 
     <div class="container-fluid mb-20 " id="hero">
@@ -45,6 +51,7 @@
             <h1 class="text-4xl mb-3 hero-title font-bold">Welcome to Weatherly<span class="text-[#F2B705] text-4xl">!</span></h1>
             <p class="text-lg mb-2">Get accurate weather forecasts for the next 5 days and plan your activities with confidence.</p>
           </div>
+          
           <!--Search Bar-->
           <div>   
             <searchBar @place-data="addPlace"/>
@@ -67,4 +74,4 @@
 
 
   </main>
-</template>
+</template>./components/Location.vue./components/userLocation.vue
