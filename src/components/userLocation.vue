656d68c7c06158c81f4456c356f8d772fb4d0ab5
female-bocelli <146786813+female-bocelli@users.noreply.github.com>
2024-03-16 17:01:31 -0400

    padding

--- src/components/userLocation.vue
+++ src/components/userLocation.vue
@@ -1,13 +1,10 @@
 <template>
     <div v-if="!loading">
-        <p v-if="error">{{ error }}</p>
-        <p v-else-if="city && weatherInfo">
-            <i class="fa-solid fa-location-dot text-[#24ceff]"></i> {{ city }}, {{ region }}, {{ country }} <br>
-            <span v-if="weatherInfo.icon"><img :src="weatherInfo.icon" width="30" alt="Weather Icon">{{ Math.round(weatherInfo.temperature) }}°C, {{ weatherInfo.condition }}</span>
-        </p>
-        
-        
-        
+      <p v-if="error">{{ error }}</p>
+      <p v-else-if="city && weatherInfo">
+        <i class="fa-solid fa-location-dot text-[#24ceff]"></i> {{ city }}, {{ region }}, {{ country }} <br>
+        <span v-if="weatherInfo.icon"><img :src="weatherInfo.icon" width="30" alt="Weather Icon">{{ Math.round(weatherInfo.temperature) }}°C, {{ weatherInfo.condition }}</span>
+      </p>
     </div>
 </template>
   
@@ -24,23 +21,18 @@
       const weatherInfo = ref(null);
   
       const getLocationInfo = async (latitude, longitude) => {
-        try {
-          const response = await fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${longitude},${latitude}.json?access_token=pk.eyJ1Ijoid2VhdGhlcmx5IiwiYSI6ImNsdHN6aWVodzB1YnYybHJtanRoNHp5cXoifQ.E1vaQJ9EQmbe8gAKfIvpsQ`);
-          const data = await response.json();
-          if (response.ok) {
-            const feature = data.features[0];
-            city.value = extractComponent(feature, 'place', 'city');
-            region.value = extractComponent(feature, 'region');
-            country.value = extractComponent(feature, 'country');
-            getWeatherInfo(city.value); // Get weather information for the city
-          } else {
-            throw new Error(data.message || 'Failed to retrieve location.');
-          }
-        } catch (err) {
-          error.value = `Error: ${err.message}`;
-        } finally {
-          loading.value = false;
+        const response = await fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${longitude},${latitude}.json?access_token=pk.eyJ1Ijoid2VhdGhlcmx5IiwiYSI6ImNsdHN6aWVodzB1YnYybHJtanRoNHp5cXoifQ.E1vaQJ9EQmbe8gAKfIvpsQ`);
+        const data = await response.json();
+        if (response.ok) {
+          const feature = data.features[0];
+          city.value = extractComponent(feature, 'place', 'city');
+          region.value = extractComponent(feature, 'region');
+          country.value = extractComponent(feature, 'country');
+          getWeatherInfo(city.value); // Get weather information for the city
+        } else {
+          error.value = data.message || 'Failed to retrieve location.';
         }
+        loading.value = false;
       };
   
       const extractComponent = (feature, type, component) => {
@@ -49,19 +41,15 @@
       };
   
       const getWeatherInfo = async (city) => {
-        try {
-          const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=b5d8dc347ec54185977173721240403&q=${city}&aqi=no`);
-          const data = await response.json();
-          if (response.ok) {
-            const temperature = data.current.temp_c;
-            const condition = data.current.condition.text;
-            const iconUrl = data.current.condition.icon;
-            weatherInfo.value = { temperature, condition, icon: iconUrl };
-          } else {
-            throw new Error(data.error.message || 'Failed to retrieve weather information.');
-          }
-        } catch (err) {
-          error.value = `Error: ${err.message}`;
+        const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=b5d8dc347ec54185977173721240403&q=${city}&aqi=no`);
+        const data = await response.json();
+        if (response.ok) {
+          const temperature = data.current.temp_c;
+          const condition = data.current.condition.text;
+          const iconUrl = data.current.condition.icon;
+          weatherInfo.value = { temperature, condition, icon: iconUrl };
+        } else {
+          error.value = data.error.message || 'Failed to retrieve weather information.';
         }
       };
   
@@ -78,7 +66,7 @@
               country.value = null;
               error.value = null;
             } else {
-              error.value = `Error: ${err.message}`;
+              error.value = err.message || 'Failed to retrieve location.';
             }
             loading.value = false;
           }
